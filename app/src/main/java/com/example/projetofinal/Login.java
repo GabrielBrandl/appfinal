9ef7b19a0568f630940ae087507dce70915e6e87
castllo <castillogabriel.cc@gmail.com>
2024-05-30 21:56:08 -0400

    Consertado erros e login com credenciais

--- app/src/main/java/com/example/projetofinal/Login.java
+++ app/src/main/java/com/example/projetofinal/Login.java
@@ -6,25 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        Button entrar;
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        entrar = (Button) findViewById(R.id.button_entrar);
+        Button btn_entrar = findViewById(R.id.button_entrar);
+        EditText txt_usuario = findViewById(R.id.eTextEmail);
+        EditText txt_senha = findViewById(R.id.eTextSenha);
 
-        entrar.setOnClickListener(new View.OnClickListener() {
+        btn_entrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivities(new Intent[]{new Intent(Login.this, Home.class)});
+                if ("admin@dominio.com".equals(txt_usuario.getText().toString()) && "123".equals(txt_senha.getText().toString())) {
+                    startActivity(new Intent(Login.this, Home.class));
+                }else {
+                    Toast.makeText(Login.this, "Credenciais inválidas", Toast.LENGTH_SHORT).show();
+                }
             }
         });
-
     }
 }
