7b4c8fc8db15d2ed99f33b29a4b6f0a3fad28713
castllo <castillogabriel.cc@gmail.com>
2024-05-30 22:32:29 -0400

    Início da implementação do carousel

---
+++ app/src/main/java/com/example/projetofinal/ImageAdapter.java
@@ -0,0 +1,45 @@
+package com.example.projetofinal;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
+    Context context;
+    ArrayList<String> arrayList;
+
+    public ImageAdapter (Context context, ArrayList<String> arrayList) {
+        this.context = context;
+        this.arrayList = arrayList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.list_item_image);
+        }
+    }
+}
